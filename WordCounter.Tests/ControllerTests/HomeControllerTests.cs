f6beeea4af2b3767c7918fb815acc95ebe5bc1cd
Stuart Gill <stuart.a.gill.gmail.com>
2018-11-30 12:39:18 -0800

    add two controller tests for the home controller

--- WordCounter.Tests/ControllerTests/HomeControllerTests.cs
+++ WordCounter.Tests/ControllerTests/HomeControllerTests.cs
@@ -8,6 +8,32 @@ namespace WordCounter.Tests
 {
     [TestClass]
     public class HomeControllerTest
+        {
+        [TestMethod]
+    public void Create_ReturnsCorrectActionType_RedirectToActionResult()
     {
+        //Arrange
+        WordCounterController controller = new WordCounterController();
+
+        //Act
+        IActionResult view = controller.Create("this is a sentence","sentence");
+
+        //Assert
+        Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
     }
+
+    [TestMethod]
+    public void Create_RedirectsToCorrectAction_Index()
+    {
+        //Arrange
+        WordCounterController controller = new WordCounterController();
+        RedirectToActionResult actionResult = controller.Create("this is a sentence","sentence") as RedirectToActionResult;
+
+        //Act
+        string result = actionResult.ActionName;
+
+        //Assert
+        Assert.AreEqual(result, "Index");
+    }
+  }
 }
